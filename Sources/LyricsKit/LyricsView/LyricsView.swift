1f62c9309d57fbd5fd7b27c9d60520de484a6f9c
Yiming Liu <YimingDesigner@gmail.com>
2023-01-24 16:35:56 +0800

    feat: support `font` and `scrollIndicator` customization

--- Sources/LyricsKit/LyricsView/LyricsView.swift
+++ Sources/LyricsKit/LyricsView/LyricsView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import ModernUI
 
 @available(macOS 13.0, *)
 public struct LyricsView: View {
@@ -13,21 +14,43 @@ public struct LyricsView: View {
     @ObservedObject var lyrics: SyncableLyrics
     let tapAction: (_ time: Double?) -> Void
     
-    public init(lyrics: SyncableLyrics, tapAction: @escaping (_ time: Double?) -> Void) {
+    let font: Font
+    let scrollIndicatorVisibility: ScrollIndicatorVisibility
+    
+    public init(lyrics: SyncableLyrics,
+                font : Font = .system(size: 22),
+                scrollIndicatorVisibility: ScrollIndicatorVisibility = .automatic,
+                tapAction: @escaping (_ time: Double?) -> Void) {
         self.lyrics = lyrics
         self.tapAction = tapAction
+        
+        self.font = font
+        self.scrollIndicatorVisibility = scrollIndicatorVisibility
     }
     
     public var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 24) {
                 ForEach(lyrics.indices, id: \.self) { index in
-                    LyricsRow(line: lyrics.lines[index], isHighlighted: index == lyrics.index ? true : false, tapAction: tapAction)
+                    let line = lyrics.lines[index]
+                    Button {
+                        tapAction(line.time)
+                    } label: {
+                        Text(line.text ?? "")
+                            .font(font)
+                            .bold()
+                            .padding(10)
+                            .foregroundColor((index == lyrics.index) ? .primary : .tertiary)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                    }
+                    .buttonStyle(ShadeButtonStyle(.wrapped, size: .huge))
                 }
             }
             .padding()
         }
+        .scrollIndicators(scrollIndicatorVisibility)
     }
+    
 }
 
 @available(macOS 13.0, *)
