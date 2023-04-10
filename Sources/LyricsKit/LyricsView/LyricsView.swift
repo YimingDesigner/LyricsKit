37c4b74ea69738c136647c8510fad9363c9bfd47
Yiming Liu <YimingDesigner@gmail.com>
2023-04-10 10:37:49 +0800

    feat: add customization modifiers
    BREAKING CHANGE: pass value via EnvironmentValues

--- Sources/LyricsKit/LyricsView/LyricsView.swift
+++ Sources/LyricsKit/LyricsView/LyricsView.swift
@@ -14,43 +14,80 @@ public struct LyricsView: View {
     @ObservedObject var lyrics: SyncableLyrics
     let tapAction: (_ time: Double?) -> Void
     
-    let font: Font
-    let scrollIndicatorVisibility: ScrollIndicatorVisibility
+    @Environment(\.font) var font
+    @Environment(\.verticalScrollIndicatorVisibility) var verticalScrollIndicatorVisibility
+    @Environment(\.scrollAnchor) var scrollAnchor
+    @Environment(\.emptyLinePlaceholder) var emptyLinePlaceholder
     
-    public init(lyrics: SyncableLyrics,
-                font: Font = .system(size: 22),
-                scrollIndicatorVisibility: ScrollIndicatorVisibility = .automatic,
-                tapAction: @escaping (_ time: Double?) -> Void) {
+    public init(lyrics: SyncableLyrics, tapAction: @escaping (_ time: Double?) -> Void) {
         self.lyrics = lyrics
         self.tapAction = tapAction
-        
-        self.font = font
-        self.scrollIndicatorVisibility = scrollIndicatorVisibility
     }
     
     public var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: 24) {
-                ForEach(lyrics.indices, id: \.self) { index in
-                    let line = lyrics.lines[index]
-                    Button {
-                        tapAction(line.time)
-                    } label: {
-                        Text(line.text ?? "")
-                            .font(font)
-                            .bold()
-                            .padding(10)
-                            .foregroundColor((index == lyrics.index) ? .primary : .tertiary)
-                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+        ScrollViewReader { scrollProxy in
+            ScrollView {
+                VStack(alignment: .leading, spacing: 10) {
+                    ForEach(lyrics.indices, id: \.self) { index in
+                        Button {
+                            tapAction(lyrics.lines[index].time)
+                        } label: {
+                            Text(lyrics.lines[index].text ?? emptyLinePlaceholder)
+                                .font(font)
+                                .bold()
+                                .padding(10)
+                                .foregroundColor((index == lyrics.index) ? .primary : .tertiary)
+                                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        }
+                        .buttonStyle(LyricsButtonStyle())
+                        .id(index)
                     }
-                    .buttonStyle(ShadeButtonStyle(.wrapped, size: .huge))
+                }
+                .padding()
+            }
+            .scrollIndicators(verticalScrollIndicatorVisibility)
+            .onChange(of: lyrics.index) { newIndex in
+                withAnimation {
+                    scrollProxy.scrollTo(newIndex, anchor: UnitPoint(x: 0.5, y: scrollAnchor))
                 }
             }
-            .padding()
+            
         }
-        .scrollIndicators(scrollIndicatorVisibility)
     }
     
+}
+
+// MARK: - Environment Values
+
+private struct ScrollAnchorKey: EnvironmentKey {
+    static let defaultValue: CGFloat = 0.1
+}
+
+private struct EmptyLinePlaceholderKey: EnvironmentKey {
+    static let defaultValue: String = "•••"
+}
+
+@available(macOS 13.0, *)
+extension EnvironmentValues {
+    var scrollAnchor: CGFloat {
+        get { self[ScrollAnchorKey.self] }
+        set { self[ScrollAnchorKey.self] = newValue }
+    }
+    var emptyLinePlaceholder: String {
+        get { self[EmptyLinePlaceholderKey.self] }
+        set { self[EmptyLinePlaceholderKey.self] = newValue }
+    }
+}
+
+@available(macOS 13.0, *)
+public extension View {
+    func scrollAnchor(_ anchor: CGFloat) -> some View {
+        environment(\.scrollAnchor, anchor)
+    }
+    
+    func emptyLinePlaceholder(_ placeholder: String) -> some View {
+        environment(\.emptyLinePlaceholder, placeholder)
+    }
 }
 
 @available(macOS 13.0, *)
